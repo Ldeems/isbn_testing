b419dcd7365963986f46d62c2e72b43760a23685
Ldeems <luke.deems@yahoo.com>
2017-08-28 14:58:24 -0400

    simple length test working

---
+++ isbn_method.rb
@@ -0,0 +1,9 @@
+def isbn_check(i)
+
+if i.length == 10
+	true
+else
+ 	false
+end 		
+
+end	
--- isbn_test.rb
+++ isbn_test.rb
@@ -6,5 +6,8 @@ def test_2_equals_2
 	assert_equal(2,2)
 end	
 
+def test_simple_legth
+	assert_equal(true, isbn_check("0471958697"))
+end	
 
 end	
