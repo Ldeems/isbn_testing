d4c492d9bdaf2cee59ca8ac49cb77022733f9fc8
Ldeems <luke.deems@yahoo.com>
2017-09-05 13:28:50 -0400

    working on csv, got it to iterate and print

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
 require "sinatra"
 require_relative "isbn_method.rb"
+require "csv"
 enable :sessions
 
 
 get "/" do
-	erb :number
+	session[:list] = CSV.read("input_isbn_file.csv")
+	erb :number, locals:{list:session[:list]}
 end
 
 post '/number' do
@@ -13,6 +15,15 @@ post '/number' do
 end
 
 get '/results' do
+
 	result = isbn_size_check(session[:number])
 	erb :results, locals:{number:session[:number], result:result}
+end	
+
+post '/csv' do
+	redirect '/cvs'
+end	
+
+get '/cvs' do
+	erb :cvs, locals:{list:session[:list]}
 end	
